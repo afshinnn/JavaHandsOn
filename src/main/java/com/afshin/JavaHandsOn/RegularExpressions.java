1596edb73eff6cc94d9f44db19efe72930802c9b
Afshin <abehzadan@oanda.com>
2019-09-22 22:56:35 -0400

    pattern from multiple strings

--- src/main/java/com/afshin/JavaHandsOn/RegularExpressions.java
+++ src/main/java/com/afshin/JavaHandsOn/RegularExpressions.java
@@ -5,11 +5,15 @@ import java.util.regex.Pattern;
 /**
  * This class includes practices on regular expressions.
  */
-public class RegularExpressions {
-    private static final Pattern junePattern = Pattern.compile(".*june.*");
-    private static final Pattern julyPattern = Pattern.compile(".*july.*");
+class RegularExpressions {
+    private static final String juneString = ".*june.*";
+    private static final String julyString = ".*july.*";
 
-    public void run() {
+    private static final Pattern junePattern = Pattern.compile(juneString);
+    private static final Pattern julyPattern = Pattern.compile(julyString);
+    private static final Pattern juneOrJulyPattern = Pattern.compile(juneString + "|" + julyString);
+
+    void run() {
         String expr1 = "123june";
 
         System.out.println(
@@ -21,9 +25,15 @@ public class RegularExpressions {
             julyPattern.pattern() + " matches with " + expr1 +" -> " +
                 singleMatch(julyPattern, expr1)
         );
+
+        System.out.println(
+            juneOrJulyPattern.pattern() + " matches with " + expr1 +" -> " +
+                singleMatch(juneOrJulyPattern, expr1)
+        );
+
     }
 
     private static boolean singleMatch(Pattern pattern, String stringToMatch) {
-        return pattern.matcher(stringToMatch).matches();
+        return pattern.matcher(stringToMatch).find();
     }
 }
