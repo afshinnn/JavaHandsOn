af73acd9aba430d3583cc2717bc85d3f4881c714
Afshin <abehzadan@oanda.com>
2019-09-30 15:46:08 -0400

    problem description for word flipper

--- src/main/java/com/afshin/JavaHandsOn/algorithms/wordflip/WordFlipper.java
+++ src/main/java/com/afshin/JavaHandsOn/algorithms/wordflip/WordFlipper.java
@@ -1,5 +1,10 @@
 package com.afshin.JavaHandsOn.algorithms.wordflip;
 
+/**
+ * Problem: Given a string as the input, write a recursive algorithm to return
+ * the reverse of a the input string.
+ * Example: Given "Monday" to be the input, then output should be "yadnoM".
+ */
 public class WordFlipper {
 
     public static String flip(String text) throws Exception {
